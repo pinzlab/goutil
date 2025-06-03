7f56672c16f044b381e3b2dcc23646c8e35401eb
PinzLab <aldaboad@gmail.com>
2025-06-03 05:14:12 -0500

    Refactor delete tracker

--- pg/track/delete.go
+++ pg/track/delete.go
@@ -1,13 +1,13 @@
 package track
 
 import (
-	"time"
+	"gorm.io/gorm"
 )
 
 // Delete represents metadata for tracking deletion operations.
 type Delete struct {
 	// DeletedAt is the timestamp when the record was deleted (soft delete).
-	DeletedAt *time.Time `gorm:"column:dat;type:timestamptz;null"`
+	DeletedAt *gorm.DeletedAt `gorm:"column:dat;type:timestamptz;null"`
 
 	// DeletedBy is the identifier of the user who deleted the record.
 	DeletedBy *int `gorm:"column:cby;type:integer;null"`
